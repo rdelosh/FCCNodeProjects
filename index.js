552751d16e0e2a1e26a64c1356b34e41795e2ca1
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2018-03-27 21:02:27 -0800

    added comment zekans

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get('/*',function(req,res){
 	
 	myurl = url.parse(req.url,true)
 	var param = myurl.path
+	const monthNames = ["January", "February", "March", "April", "May", "June",
+				  "July", "August", "September", "October", "November", "December"
+					];
 
 
 	if(param==="/"){
@@ -20,31 +23,43 @@ app.get('/*',function(req,res){
 		//return json
 		res.setHeader('Content-Type', 'application/json');
 		try{
+			if(param.includes('%20')){
+				var segments = param.split('%20')
 		
-			var segments = param.split('%20')
-		
-		var paramDay = segments[1].substring(0,segments.length-1)
-		const monthNames = ["January", "February", "March", "April", "May", "June",
-		  "July", "August", "September", "October", "November", "December"
-			];
-		var paramMonth = segments[0].substring(1,segments[0].length)
-		monthNames.map(function(month,index){
-			if(paramMonth.toUpperCase()===month.toUpperCase()){
-				paramMonth=index
+				var paramDay = segments[1].substring(0,segments.length-1)
+				
+				var paramMonth = segments[0].substring(1,segments[0].length)
+				monthNames.map(function(month,index){
+					if(paramMonth.toUpperCase()===month.toUpperCase()){
+						paramMonth=index
+					}
+				})
+				var paramYear = segments[2]
+				
+				var newdate = new Date(paramYear,paramMonth,paramDay,0,0,0,0)
+				console.log(newdate)
+
+
+				if(typeof newdate!=undefined && newdate!=null){
+					res.send(JSON.stringify(
+						{unix:newdate.getTime(),
+						naturaldate:monthNames[paramMonth]+" "+paramDay+", "+
+								paramYear}))	
+				}
+			}else{
+				param = param.substring(1,param.length)
+				console.log(param)
+				var newdate = new Date(param)
+				//zekans
+				
+				
+				console.log(newdate.toString("MM dd"))
+				res.send(JSON.stringify(
+						{unix:newdate.getTime(),
+						naturaldate:monthNames[newdate.getMonth()]+" "+newdate.getDay()+", "+
+								newdate.getYear()}))
 			}
-		})
-		var paramYear = segments[2]
 		
-		var newdate = new Date(paramYear,paramMonth,paramDay,0,0,0,0)
-		console.log(newdate)
-
-
-		if(typeof newdate!=undefined && newdate!=null){
-			res.send(JSON.stringify(
-				{unix:newdate.getTime(),
-				naturaldate:monthNames[paramMonth]+" "+paramDay+", "+
-						paramYear}))	
-		}
 		
 		}catch(e){
 			res.send(JSON.stringify({unix:null,naturaldate:null}))	
